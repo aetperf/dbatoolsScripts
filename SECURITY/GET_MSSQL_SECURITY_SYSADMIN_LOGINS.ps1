2beae6200a04d7016eeefc21373745ee79054a4e
Romain FERRATON <adm_rferraton@gra.intra.groupama.fr>
2021-11-12 21:16:14 +0100

    init v0

---
+++ SECURITY/GET_MSSQL_SECURITY_SYSADMIN_LOGINS.ps1
@@ -0,0 +1,60 @@
+Import-Module SqlServer
+
+Function Parse-ServerGroup($serverGroup)
+{
+$results = $serverGroup.RegisteredServers;
+foreach($group in $serverGroup.ServerGroups)
+{
+$results += Parse-ServerGroup -serverGroup $group;
+}
+return $results;
+}
+Function Get-ServerList ([string]$cmsName, [string]$serverGroup, [switch]$recurse)
+{
+$connectionString = "data source=$cmsName;initial catalog=master;integrated security=sspi;"
+$sqlConnection = New-Object ("System.Data.SqlClient.SqlConnection") $connectionstring
+$conn = New-Object ("Microsoft.SQLServer.Management.common.serverconnection") $sqlconnection
+$cmsStore = New-Object ("Microsoft.SqlServer.Management.RegisteredServers.RegisteredServersStore") $conn
+$cmsRootGroup = $cmsStore.ServerGroups["DatabaseEngineServerGroup"].ServerGroups[$serverGroup]
+
+if($recurse)
+{
+return Parse-ServerGroup -serverGroup $cmsRootGroup | select ServerName
+}
+else
+{
+return $cmsRootGroup.RegisteredServers | select ServerName
+}
+}
+$TempDir = "."
+$ExportDir = "\\GRASRLYOTAB1\tableauIN\Security"
+$CentralManagementServer = ".\DBA01"
+$serverList = Get-ServerList -cmsName $CentralManagementServer -serverGroup ALL -recurse
+
+$serverList | Format-Table
+
+#Load SMO assemblies
+
+$MS='Microsoft.SQLServer'
+@('.SMO', '.Management.RegisteredServers', '.ConnectionInfo') |
+ foreach-object {if ([System.Reflection.Assembly]::LoadWithPartialName("$MS$_") -eq $null) {"missing SMO component $MS$_"}}
+ 
+$extractdate=$(get-date).ToLongDateString()
+
+ 
+$My="$ms.Management.Smo" #
+$serverList|
+ Foreach-object {new-object ("$My.Server") $_.ServerName } | # create an SMO server object
+ Where-Object {$_.ServerType -ne $null} | # did you positively get the server?
+ Foreach-object {$_.Logins } | #logins for every server successfully reached 
+ where {$_.IsMember("sysadmin")} | #only sysadmin
+ Select-object @{Name="Server"; Expression={$_.parent}}, Name, DefaultDatabase , CreateDate, DateLastModified , IsDisabled |
+ Export-Csv -Path $TempDir\sysadmins_logins.csv -Delimiter '|' -NoTypeInformation -Force
+ 
+ $csvData=Import-csv $TempDir\sysadmins_logins.csv -Delimiter '|'
+ $thingToImport = [psobject]$csvData
+ 
+  Invoke-Sqlcmd -Query "TRUNCATE TABLE DBATOOLS.dbo.SysAdminLogins;" -ServerInstance "localhost\DBA01"
+
+ 
+ Write-SqlTableData -ServerInstance "localhost\DBA01" -DatabaseName "DBATOOLS" -SchemaName "dbo" -TableName "SysAdminLogins" -InputData $thingToImport -Force
