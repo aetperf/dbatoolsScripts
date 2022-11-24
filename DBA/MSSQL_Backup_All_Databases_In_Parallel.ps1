4d2a400869d066cbd3c5919919bad2d95e11f04a
Romain Ferraton <rferraton@users.noreply.github.com>
2022-11-24 17:42:48 +0100

    change log filename template

--- DBA/MSSQL_Backup_All_Databases_In_Parallel.ps1
+++ DBA/MSSQL_Backup_All_Databases_In_Parallel.ps1
@@ -40,6 +40,9 @@
         Dependencies : 
             Install-Module Logging
             Install-Module dbatools
+            Install-Module JoinModule
+
+        Compatibility : Powershell 7.3+
 
     .LINK
         
@@ -51,132 +54,135 @@
         This will perform a Full parallel backups of all databases of the MySQLServerInstance Instance in the S:\BACKUPS Directory with backup files slitted into 4 parts and log will be console displayed
     #>
 
-param 
-(
-    [Parameter(Mandatory)] [string] $SqlInstance,
-    [Parameter(Mandatory)] [ValidateSet('Full','Diff','Log')] [string] $BackupType,
-    [Parameter(Mandatory)] [string] $BackupDirectory,
-    [Parameter()] [Int16] $Degree = 4,
-    [Parameter()] [Int16] $FileCount = 4,
-    [Parameter()] [Int16] $Timeout = 3600,
-    [Parameter()] [string] $LogLevel = "INFO",
-    [Parameter()] [string] $LogDirectory
-)
-
-Set-LoggingDefaultLevel -Level $LogLevel
-Add-LoggingTarget -Name Console -Configuration @{
-    ColorMapping = @{
-        DEBUG = 'Gray'
-        INFO  = 'White'
-        ERROR  = 'DarkRed'
-    }
-}
-
-if ($PSBoundParameters.ContainsKey('LogDirectory'))
-{   
-    $TimestampLog=Get-Date -UFormat "%Y-%m-%dT%H%M%S"
-    $LogfileName="dbatools_backup_database_" + $TimestampLog + ".log"
-    $LogFile= Join-DbaPath -Path $LogDirectory -Child $LogfileName
-    Add-LoggingTarget -Name File -Configuration @{Level = 'INFO'; Path = $LogFile}
-    Write-Log -Level INFO -Message "Log File : $LogFile"
-}
-
-Write-Log -Level INFO -Message "Parameter SQLInstance : ${SqlInstance}"
-Write-Log -Level INFO -Message "Parameter BackupType : ${BackupType}"
-Write-Log -Level INFO -Message "Parameter BackupDirectory : ${BackupDirectory}"
-Write-Log -Level INFO -Message "Parameter Degree : ${Degree}"
-Write-Log -Level INFO -Message "Parameter FileCount : ${FileCount}"
-Write-Log -Level INFO -Message "Parameter Timeout : ${Timeout}"
-
-# Select Normal and Open Writable databases exluding system databases except msdb
-
-
-Write-Log -Level DEBUG -Message "Databases found by Get-DbaDatabase detail in body" -Body $Databases
-
-switch ($BackupType) 
-    {
-        "Full" 
-            {
-                $BackupExtension="bak"  
-                $Databases = Get-DbaDatabase -SqlInstance $SqlInstance -ExcludeDatabase "tempdb","model" | Where-Object {($_.IsUpdateable) -and ($_.Status -ilike "Normal*")}
-            }
-        "Diff" 
-            {
-                $BackupExtension="diff" 
-                $Databases = Get-DbaDatabase -SqlInstance $SqlInstance -ExcludeDatabase "tempdb","model","master" | Where-Object {($_.IsUpdateable) -and ($_.Status -ilike "Normal*")}
-            }
-        "Log"  
-            {
-                $BackupExtension="trn"  
-                $Databases = Get-DbaDatabase -SqlInstance $SqlInstance -ExcludeDatabase "tempdb","model" | Where-Object { ($_.IsUpdateable) -and ($_.Status -ilike "Normal*") -and ($_.RecoveryModel -ne "Simple")}
-            }
-
-    }
+    param 
+    (
+        [Parameter(Mandatory)] [string] $SqlInstance,
+        [Parameter(Mandatory)] [ValidateSet('Full','Diff','Log')] [string] $BackupType,
+        [Parameter(Mandatory)] [string] $BackupDirectory,
+        [Parameter()] [Int16] $Degree = 4,
+        [Parameter()] [Int16] $FileCount = 4,
+        [Parameter()] [Int16] $Timeout = 3600,
+        [Parameter()] [string] $LogLevel = "INFO",
+        [Parameter()] [string] $LogDirectory
+    )
     
-
-
-# A concurrent dictionnary to manage log and returns from parallel backups
-$ResultsObjects = [System.Collections.Concurrent.ConcurrentDictionary[string,System.Object]]::new()
-
-$duration=(Measure-Command{
-
-    $SilentdbatoolsResults = $Databases | Sort-Object -Property Size -Descending | ForEach-Object -Parallel {
-
-    $ResultsDict = $using:ResultsObjects #mapped to the $ResultsObjects ConcurrentDictionnary (Thread Safe)
-    $Database=$_.Name
-    $SqlInstance=$_.SqlInstance
-    $BackupDirectory=$using:BackupDirectory
-    $FileCount=$using:FileCount
-    $BackupType=$using:BackupType
-
-    try {            
-            $resbackup = Backup-DbaDatabase -SqlInstance $SqlInstance -Database $Database -Type $BackupType -CompressBackup -Checksum -Verify -FileCount $FileCount -FilePath "${BackupDirectory}\servername\instancename\dbname\backuptype\servername_dbname_backuptype_timestamp.${BackupExtension}" -TimeStampFormat "yyyyMMdd_HHmm" -ReplaceInName -CreateFolder #-ErrorAction Stop -EnableException
-            $silentres = $ResultsDict.TryAdd($Database,$resbackup)
+    
+    Set-LoggingDefaultLevel -Level $LogLevel
+    Add-LoggingTarget -Name Console -Configuration @{
+        ColorMapping = @{
+            DEBUG = 'Gray'
+            INFO  = 'White'
+            ERROR  = 'DarkRed'
         }
-        catch  {
-            Write-Host "[ERROR] - Backup $BackupType of $Database Failed"
-            $silentres=$ResultsDict.TryAdd($Database,$resbackup)
-        } 
-    } -ThrottleLimit $Degree -TimeoutSeconds $Timeout 
-} ).TotalSeconds
-     
-    # Get Results from $ResultsObjects ConcurrentDictionary and find issues
-    $ResultsCount = $ResultsObjects.Count
+    }
     
-   
-    $resultinfos = $ResultsObjects.Values | Select-Object ComputerName, InstanceName, Database,Type,Software , Start, End, Duration,TotalSize, CompressedBackupSize, Verified, Backupcomplete, Backupfilescount, BackupPath,Script  | Sort-Object -Property Database
+    if ($PSBoundParameters.ContainsKey('LogDirectory'))
+    {   
+        $TimestampLog=Get-Date -UFormat "%Y-%m-%dT%H%M%S"
+        mkdir $LogDirectory -Force
+        $LogfileName="dbatools_backup_database_" + $BackupType + "_"+ $TimestampLog + ".log"
+        $LogFile= Join-DbaPath -Path $LogDirectory -Child $LogfileName
+        Add-LoggingTarget -Name File -Configuration @{Level = 'INFO'; Path = $LogFile}
+        Write-Log -Level INFO -Message "Log File : $LogFile"
+    }
     
-    $ResultAllDb = $ResultsObjects.Keys | Foreach-Object { [PSCustomObject]@{'Database' = $_ }}
-    $DatabasesBackupProblems = $ResultAllDb | LeftJoin $resultinfos -On Database | where-object{$null -eq $_.BackupComplete -Or $_BackupComplete -eq $False}
+    Write-Log -Level INFO -Message "Parameter SQLInstance : ${SqlInstance}"
+    Write-Log -Level INFO -Message "Parameter BackupType : ${BackupType}"
+    Write-Log -Level INFO -Message "Parameter BackupDirectory : ${BackupDirectory}"
+    Write-Log -Level INFO -Message "Parameter Degree : ${Degree}"
+    Write-Log -Level INFO -Message "Parameter FileCount : ${FileCount}"
+    Write-Log -Level INFO -Message "Parameter Timeout : ${Timeout}"
     
-    $DatabasesBackupProblemsCount = $DatabasesBackupProblems.Count
-
-    # Log Results
-    foreach ($resultinfo in $resultinfos) {
-        $Message= "ComputerName : " + $resultinfo.ComputerName + " | InstanceName : " + $resultinfo.InstanceName + " | Database : " + $resultinfo.Database + " | Type : " + $resultinfo.Type + " | Start : " + $resultinfo.Start + " | End : " + $resultinfo.End + " | Duration : " + $resultinfo.Duration  + " | TotalSize : " + $resultinfo.TotalSize + " | CompressedBackupSize : " + $resultinfo.CompressedBackupSize + " | Verified : " + $resultinfo.Verified + " | Backupfilescount : " + $resultinfo.Backupfilescount + " | BackupPath : " + $resultinfo.BackupPath+ " | BackupScript : " + $resultinfo.Script
-        if ($resultinfo.Backupcomplete)
+    # Select Normal and Open Writable databases exluding system databases except msdb
+    
+    
+    #Write-Log -Level DEBUG -Message "Databases found by Get-DbaDatabase detail in body" -Body $Databases
+    
+    switch ($BackupType) 
         {
-            Write-Log -Level INFO -Message $Message
-        }
-
-    }
+            "Full" 
+                {
+                    $BackupExtension="bak"  
+                    $Databases = Get-DbaDatabase -SqlInstance $SqlInstance -ExcludeDatabase "tempdb","model" | Where-Object {($_.IsUpdateable) -and ($_.Status -ilike "Normal*")}
+                }
+            "Diff" 
+                {
+                    $BackupExtension="diff" 
+                    $Databases = Get-DbaDatabase -SqlInstance $SqlInstance -ExcludeDatabase "tempdb","model","master" | Where-Object {($_.IsUpdateable) -and ($_.Status -ilike "Normal*")}
+                }
+            "Log"  
+                {
+                    $BackupExtension="trn"  
+                    $Databases = Get-DbaDatabase -SqlInstance $SqlInstance -ExcludeDatabase "tempdb","model" | Where-Object { ($_.IsUpdateable) -and ($_.Status -ilike "Normal*") -and ($_.RecoveryModel -ne "Simple")}
+                }
     
-    foreach ($resultinfo in $DatabasesBackupProblems)
-    {
-        $Message= "InstanceName : " + $SqlInstance + " | Database : " + $resultinfo.Database + " | Type : " + $BackupType
-        Write-Log -Level ERROR -Message $Message
-    }
-
-    # change Return Code if at least one backup had a problem
-    if ($DatabasesBackupProblems.Count -gt 0)
-    {
-        Start-Sleep -Seconds 1       
-        Write-Log -Level INFO -Message "${ResultsCount} ${BackupType} Backups was tried with ${DatabasesBackupProblemsCount} failure(s) in ${duration} seconds `n"
-        Write-Log -Level ERROR -Message "${DatabasesBackupProblemsCount} Database Backup(s) had a problem"
-        exit 2
-    }
-
-    Write-Log -Level INFO -Message "${ResultsCount} ${BackupType} Backups SUCCESSFULLY COMPLETED in ${duration} seconds"
-
-    return $ResultsObjects
-
+        }
+        
+    
+    
+    # A concurrent dictionnary to manage log and returns from parallel backups
+    $ResultsObjects = [System.Collections.Concurrent.ConcurrentDictionary[string,System.Object]]::new()
+    
+    $duration=(Measure-Command{
+    
+        $SilentdbatoolsResults = $Databases | Sort-Object -Property Size -Descending | ForEach-Object -Parallel {
+    
+        $ResultsDict = $using:ResultsObjects #mapped to the $ResultsObjects ConcurrentDictionnary (Thread Safe)
+        $Database=$_.Name
+        $SqlInstance=$_.SqlInstance
+        $BackupDirectory=$using:BackupDirectory
+        $FileCount=$using:FileCount
+        $BackupType=$using:BackupType
+    
+        try {            
+                $resbackup = Backup-DbaDatabase -SqlInstance $SqlInstance -Database $Database -Type $BackupType -CompressBackup -Checksum -Verify -FileCount $FileCount -FilePath "${BackupDirectory}\servername\instancename\dbname\backuptype\servername_dbname_backuptype_timestamp.${BackupExtension}" -TimeStampFormat "yyyyMMdd_HHmm" -ReplaceInName -CreateFolder #-ErrorAction Stop -EnableException
+                $silentres = $ResultsDict.TryAdd($Database,$resbackup)
+            }
+            catch  {
+                Write-Host "[ERROR] - Backup $BackupType of $Database Failed"
+                $silentres=$ResultsDict.TryAdd($Database,$resbackup)
+            } 
+        } -ThrottleLimit $Degree -TimeoutSeconds $Timeout 
+    } ).TotalSeconds
+         
+        # Get Results from $ResultsObjects ConcurrentDictionary and find issues
+        $ResultsCount = $ResultsObjects.Count
+        
+       
+        $resultinfos = $ResultsObjects.Values | Select-Object ComputerName, InstanceName, Database,Type,Software , Start, End, Duration,TotalSize, CompressedBackupSize, Verified, Backupcomplete, Backupfilescount, BackupPath,Script  | Sort-Object -Property Database
+        
+        $ResultAllDb = $ResultsObjects.Keys | Foreach-Object { [PSCustomObject]@{'Database' = $_ }}
+        $DatabasesBackupProblems = $ResultAllDb | LeftJoin $resultinfos -On Database | where-object{$null -eq $_.BackupComplete -Or $_BackupComplete -eq $False}
+        
+        $DatabasesBackupProblemsCount = $DatabasesBackupProblems.Count
+    
+        # Log Results
+        foreach ($resultinfo in $resultinfos) {
+            $Message= "ComputerName : " + $resultinfo.ComputerName + " | InstanceName : " + $resultinfo.InstanceName + " | Database : " + $resultinfo.Database + " | Type : " + $resultinfo.Type + " | Start : " + $resultinfo.Start + " | End : " + $resultinfo.End + " | Duration : " + $resultinfo.Duration  + " | TotalSize : " + $resultinfo.TotalSize + " | CompressedBackupSize : " + $resultinfo.CompressedBackupSize + " | Verified : " + $resultinfo.Verified + " | Backupfilescount : " + $resultinfo.Backupfilescount + " | BackupPath : " + $resultinfo.BackupPath+ " | BackupScript : " + $resultinfo.Script
+            if ($resultinfo.Backupcomplete)
+            {
+                Write-Log -Level INFO -Message $Message
+            }
+    
+        }
+        
+        foreach ($resultinfo in $DatabasesBackupProblems)
+        {
+            $Message= "InstanceName : " + $SqlInstance + " | Database : " + $resultinfo.Database + " | Type : " + $BackupType
+            Write-Log -Level ERROR -Message $Message
+        }
+    
+        # change Return Code if at least one backup had a problem
+        if ($DatabasesBackupProblems.Count -gt 0)
+        {
+            Start-Sleep -Seconds 1       
+            Write-Log -Level INFO -Message "${ResultsCount} ${BackupType} Backups was tried with ${DatabasesBackupProblemsCount} failure(s) in ${duration} seconds `n"
+            Write-Log -Level ERROR -Message "${DatabasesBackupProblemsCount} Database Backup(s) had a problem"
+            exit 2
+        }
+    
+        Write-Log -Level INFO -Message "${ResultsCount} ${BackupType} Backups SUCCESSFULLY COMPLETED in ${duration} seconds"
+    
+        #return $ResultsObjects
+    
+    
