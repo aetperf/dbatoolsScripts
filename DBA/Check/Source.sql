b508b94086bc2adf2992db7d88cc88c5077f860c
PierreAntoineAP <pierre-antoine.collet@architecture-performance.fr>
2025-02-12 11:48:21 +0100

    Update SP checkindexesdetails avec le detail des indexes manquant

--- DBA/Check/Source.sql
+++ DBA/Check/Source.sql
@@ -13,7 +13,10 @@ CREATE TABLE [dba].[IndexLandscape](
 	[SchemaName] [sysname] NOT NULL,
 	[TableName] [sysname] NOT NULL,
 	[IndexName] [nvarchar](200) NOT NULL,
+	[IndexType] [sysname] NOT NULL,
 	[IndexedColumns] [nvarchar](max) NOT NULL,
+	[IncludeColumns] [nvarchar](max),
+	[FilterCondition] [nvarchar](max),
  CONSTRAINT [PK_IndexLandscape] PRIMARY KEY CLUSTERED 
 (
 	[SchemaName] ASC,
@@ -25,16 +28,33 @@ GO
 
 --Populate table [dba].[IndexLandscape] 
 INSERT INTO [dba].[IndexLandscape]
-SELECT 
-	s.name AS SchemaName,  
-	t.name AS TableName,
-	i.name AS IndexName,
-	STRING_AGG(c.name, ', ') WITHIN GROUP (ORDER BY ic.index_column_id) AS IndexedColumns
-FROM sys.indexes i
-	JOIN sys.tables t ON i.object_id = t.object_id
-	JOIN sys.schemas s ON t.schema_id = s.schema_id
-	JOIN sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
-	JOIN sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
-WHERE i.type > 0 -- Exclut les index "HEAP"
-GROUP BY s.name, t.name, i.name;
+        SELECT 
+            s.name AS SchemaName,  
+            t.name AS TableName,
+            i.name AS IndexName,
+            i.type_desc AS IndexType,
+            STRING_AGG(
+                CASE 
+                    WHEN i.type_desc LIKE '%COLUMNSTORE%' THEN c.name
+                    WHEN ic.is_included_column = 0 THEN c.name 
+                END, ', ') 
+                WITHIN GROUP (ORDER BY ic.index_column_id) 
+                AS IndexedColumns,
+            STRING_AGG(
+                CASE 
+                    WHEN i.type_desc LIKE '%COLUMNSTORE%' THEN NULL
+                    WHEN ic.is_included_column = 1 THEN c.name 
+                END, ', ') 
+                WITHIN GROUP (ORDER BY ic.index_column_id) 
+                AS IncludeColumns,
+            i.filter_definition AS FilterCondition
+
+        FROM sys.indexes i
+        JOIN sys.tables t ON i.object_id = t.object_id
+        JOIN sys.schemas s ON t.schema_id = s.schema_id  
+        JOIN sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
+        JOIN sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
+        WHERE i.type > 0  -- Exclut les index HEAP (tables sans index clusterisé)
+        AND s.name LIKE '%'
+        GROUP BY s.name, t.name, i.name, i.type_desc, i.filter_definition
 
