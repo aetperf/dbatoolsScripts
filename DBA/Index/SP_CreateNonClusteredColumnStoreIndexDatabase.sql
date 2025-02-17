9d16f9f0cd1c2b23ab9d17c5ecc4233d898c0b7f
PierreAntoineAP <pierre-antoine.collet@architecture-performance.fr>
2025-02-17 16:12:53 +0100

    update

--- DBA/Index/SP_CreateNonClusteredColumnStoreIndexDatabase.sql
+++ DBA/Index/SP_CreateNonClusteredColumnStoreIndexDatabase.sql
@@ -1,16 +1,14 @@
---EXEC dbo.GenerateDatabaseColumnstoreIndex @schemaNameLike='alert', @tableNameLike='Alert%', @fileGroup='INDEX_CI',@debug=1, @execute=1,@continue=0
-
 CREATE OR ALTER PROCEDURE dbo.GenerateDatabaseColumnstoreIndex
-	@schemaNameLike NVARCHAR(100)='%',		-- Instruction SQL dans le like pour le nom du sch�ma par d�faut '%'
-    @tableNameLike NVARCHAR(100)='%',		-- Instruction SQL dans le like pour le nom de la table par d�faut '%'
+	@schemaNameLike NVARCHAR(100)='%',		-- Instruction SQL dans le like pour le nom du schéma par défaut '%'
+    @tableNameLike NVARCHAR(100)='%',		-- Instruction SQL dans le like pour le nom de la table par défaut '%'
 	@indexName NVARCHAR(100)='<TableName>_NCCI_I0',				--Nom de l'index
-	@MinRow BIGINT=-1,						--Nombre minimum de ligne pour cr�er le non clustered columnstore index par d�faut -1
-	@MaxRow BIGINT=1000000000000,				--Nombre maximum de ligne pour cr�er le non clustered columnstore index par d�faut -1
+	@MinRow BIGINT=-1,						--Nombre minimum de ligne pour créer le non clustered columnstore index par défaut -1
+	@MaxRow BIGINT=1000000000000,			--Nombre maximum de ligne pour créer le non clustered columnstore index par défaut -1
 	@orderClause NVARCHAR(1000) = '',		--(Optionnel) Liste de colonnes pour trier le columnstore index sur les colonnes de la liste (Example : [col1],[col2]
-	@fileGroup NVARCHAR(100) = 'Primary',	--(Optionnel) Nom du groupe de fichier o� sera cr�� l'index
-	@force BIT = 0,							--(Optionnel) Si option activ�, suppresion et reconstruction de l'index
-	@execute BIT = 1,						--(Optionnel) Param�tre d'execution de l'ordre SQL par d�faut 1
-    @debug BIT = 1,							--(Optionnel) Param�tre de debug par d�faut 1
+	@fileGroup NVARCHAR(100) = 'Primary',	--(Optionnel) Nom du groupe de fichier où sera créé l'index
+	@force BIT = 0,							--(Optionnel) Si option activé, suppresion et reconstruction de l'index
+	@execute BIT = 1,						--(Optionnel) Paramétre d'execution de l'ordre SQL par défaut 1
+    @debug BIT = 1,							--(Optionnel) Paramétre de debug par défaut 1
 	@continue BIT = 1						--(Optionnel) Paramètre pour continuer l'execution si l'on rencontre une erreur avec valeur par défaut de 1
 
 AS
@@ -36,8 +34,8 @@ BEGIN
     INNER JOIN 
         sys.partitions p ON t.object_id = p.object_id
     WHERE 
-        t.is_ms_shipped = 0  -- Exclure les tables syst�me
-        AND s.name LIKE @schemaNameLike  -- Filtrer par sch�ma (si n�cessaire, remplace '%' par le sch�ma)
+        t.is_ms_shipped = 0  -- Exclure les tables système
+        AND s.name LIKE @schemaNameLike  -- Filtrer par schéma (si nécessaire, remplace '%' par le schéma)
         AND t.name LIKE @tableNameLike  -- Filtrer par nom de table (si besoin, remplace '%' par le pattern)
         AND p.index_id=1
         AND p.data_compression_desc!='COLUMNSTORE'
