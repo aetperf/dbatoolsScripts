0a84cc91c5dd70f5cb436f4fa034bc0a37dbd45d
PierreAntoineAP <pierre-antoine.collet@architecture-performance.fr>
2025-03-06 10:51:00 +0100

    commentaire sp_dropsnapshot

--- DBA/DBSnapshot/sp_dropdbsnapshot .sql
+++ DBA/DBSnapshot/sp_dropdbsnapshot .sql
@@ -1,17 +1,22 @@
 CREATE OR ALTER PROCEDURE sp_dropdbsnapshot 
-    @dbsnapshotname sysname, 
-    @execute bit = 0, 
-    @force bit = 0, 
-    @debug bit = 1, 
-    @continueOnError bit = 1
+    @dbsnapshotname     sysname,                    -- Nom du snapshot
+    @execute            BIT = 0,                    -- 1 => exécute la suppression du snapshot
+    @force              BIT = 0,                    -- 1 => Supprime les connexions en cours sur le snapshot
+    @debug              BIT = 1,                    -- 1 => affiche des messages de débogage
+    @continueOnError    BIT = 1                     -- 1 => continue malgré les erreurs (erreurs « légères »)
 WITH EXECUTE AS OWNER
 AS
 BEGIN
-    DECLARE @err INT = 0;
-	DECLARE @sql VARCHAR(MAX);
-	DECLARE @session_id INT;
+    SET NOCOUNT ON;
 
-    -- Vérifier si le snapshot existe
+    DECLARE @err            INT = 0;
+	DECLARE @sql            NVARCHAR(MAX);
+	DECLARE @session_id     INT;
+
+     
+    -----------------------------------------------------------------------
+    -- 1. Vérifier si le snapshot existe
+    -----------------------------------------------------------------------
     IF NOT EXISTS (
         SELECT 1 
         FROM sys.databases 
@@ -34,7 +39,9 @@ BEGIN
 			END
     END
 	
-    -- Si on demande de forcer, mettre la base en mode SINGLE_USER et déconnecter les utilisateurs
+    -----------------------------------------------------------------------
+    -- 2. Suppression des connexions en cours sur le snapshot
+    -----------------------------------------------------------------------
     IF @force = 1 AND @err = 0
     BEGIN
         --BEGIN TRY
@@ -76,13 +83,17 @@ BEGIN
         END CATCH*/
     END
 
-    -- Si tout est ok, on supprime le snapshot
-    IF @err = 0
+    -----------------------------------------------------------------------
+    -- 3. Exécuter la commande de suppression si @execute = 1
+    -----------------------------------------------------------------------
+    IF @err = 0 and @execute = 1
     BEGIN
         BEGIN TRY
 			IF @debug = 1
 				RAISERROR('Suppression du database snapshot %s', 1, 1, @dbsnapshotname) WITH NOWAIT;
-            EXEC('DROP DATABASE ' + QUOTENAME(@dbsnapshotname));
+			SET @sql = 'DROP DATABASE '+QUOTENAME(@dbsnapshotname)+'';
+			PRINT @sql
+            EXEC sp_executesql @sql;
         END TRY
         BEGIN CATCH
             SET @err = 1;
@@ -98,17 +109,5 @@ BEGIN
 			END
 		END CATCH
     END
-	/*
-    -- Retourner 0 si on continue malgré l'erreur ou pas d'erreur
-    IF @err = 0 OR @continueOnError = 1
-    BEGIN
-        PRINT 'Opération réussie.';
-        RETURN 0;
-    END
-    ELSE
-    BEGIN
-        PRINT 'Opération échouée.';
-        RETURN 1;
-    END*/
+	
 END
-GO
