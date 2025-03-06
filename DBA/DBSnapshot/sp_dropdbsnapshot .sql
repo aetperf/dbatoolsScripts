b643fa2a6ced15ceaea0b2ababe5aaa776f4f0ec
Romain Ferraton <16419423+rferraton@users.noreply.github.com>
2025-03-06 23:31:03 +0100

    rewrite
    - english messages and comments
    - manage @execute,@force, @debug, @continueOnError correctly
    - return 0 if OK or if @continueOnError=1
    - kill sessions only if @execute=1

--- DBA/DBSnapshot/sp_dropdbsnapshot .sql
+++ DBA/DBSnapshot/sp_dropdbsnapshot .sql
@@ -1,113 +1,94 @@
 CREATE OR ALTER PROCEDURE sp_dropdbsnapshot 
-    @dbsnapshotname     sysname,                    -- Nom du snapshot
-    @execute            BIT = 0,                    -- 1 => exécute la suppression du snapshot
-    @force              BIT = 0,                    -- 1 => Supprime les connexions en cours sur le snapshot
-    @debug              BIT = 1,                    -- 1 => affiche des messages de débogage
-    @continueOnError    BIT = 1                     -- 1 => continue malgré les erreurs (erreurs « légères »)
+    @dbsnapshotname     sysname,                    -- Snapshot Name
+    @execute            BIT = 0,                    -- 1 => Execute drop snapshot
+    @force              BIT = 0,                    -- 1 => kill existing sessions on the snapshot db if any
+    @debug              BIT = 1,                    -- 1 => debug messages are printed
+    @continueOnError    BIT = 1                     -- 1 => continue if any error. the procedure will return 0 even if error are encountered
 WITH EXECUTE AS OWNER
 AS
 BEGIN
-    SET NOCOUNT ON;
+    SET NOCOUNT, XACT_ABORT ON;
 
-    DECLARE @err            INT = 0;
 	DECLARE @sql            NVARCHAR(MAX);
+	DECLARE @ErrMsg			NVARCHAR(MAX);
 	DECLARE @session_id     INT;
 
      
     -----------------------------------------------------------------------
-    -- 1. Vérifier si le snapshot existe
+    -- 1. Check if the snapshot exists
     -----------------------------------------------------------------------
     IF NOT EXISTS (
         SELECT 1 
         FROM sys.databases 
         WHERE QUOTENAME(name) = QUOTENAME(@dbsnapshotname) 
-	AND source_database_id is not null -- Pour etre sur que la base passée en paramètre est bien un snapshot et pas une base réelle
+	AND source_database_id is not null -- to be sure the snahoshotname given in parameter is a snapshot and not a real database
     )
     BEGIN
 		IF @continueOnError=1
 			BEGIN
-				IF @debug=1
-					RAISERROR('La snapshot %s n''existe pas', 1, 1, @dbsnapshotname) WITH NOWAIT;
-				SET @err = 0;
+				RAISERROR('The snapshot %s does not exist', 1, 1, @dbsnapshotname) WITH NOWAIT;
+				RETURN 0
 			END
 		ELSE
 			BEGIN
-				IF @debug=1
-					RAISERROR('La snapshot %s n''existe pas', 16, 1, @dbsnapshotname) WITH NOWAIT;
-				SET @err = 1;
-				RETURN
+				RAISERROR('The snapshot %s does not exist', 16, 1, @dbsnapshotname) WITH NOWAIT;
+				RETURN 1
 			END
     END
 	
     -----------------------------------------------------------------------
-    -- 2. Suppression des connexions en cours sur le snapshot
+    -- 2. Kill sessions on the snapshot database if any
     -----------------------------------------------------------------------
-    IF @force = 1 AND @err = 0
+    IF @force = 1 
     BEGIN
-        --BEGIN TRY
-            -- Trouver toutes les sessions utilisant le snapshot
-            DECLARE session_cursor CURSOR FOR
-            SELECT session_id
-            FROM sys.dm_exec_sessions
-            WHERE database_id = DB_ID(@dbsnapshotname);
+        
+        DECLARE session_cursor CURSOR FOR
+        SELECT session_id
+        FROM sys.dm_exec_sessions
+        WHERE database_id = DB_ID(@dbsnapshotname);
 
-            OPEN session_cursor;
+        OPEN session_cursor;
+        FETCH NEXT FROM session_cursor INTO @session_id;
+
+        WHILE @@FETCH_STATUS = 0
+        BEGIN            
+            SET @sql = 'KILL ' + CAST(@session_id AS NVARCHAR(10));
+            IF @execute=1
+				EXEC sp_executesql @sql;
+            IF @debug=1
+				RAISERROR('kill session %d', 1, 1, @session_id) WITH NOWAIT;
             FETCH NEXT FROM session_cursor INTO @session_id;
-
-            WHILE @@FETCH_STATUS = 0
-            BEGIN
-                -- Tuer la session
-                SET @sql = 'KILL ' + CAST(@session_id AS NVARCHAR(10));
-                EXEC sp_executesql @sql;
-                IF @debug=1
-					RAISERROR('Suppression de la session %s, SQL : %s', 1, 1, @session_id,@sql) WITH NOWAIT;
-                FETCH NEXT FROM session_cursor INTO @session_id;
-            END
-
-            CLOSE session_cursor;
-            DEALLOCATE session_cursor;
-        /*END TRY
-        BEGIN CATCH
-            SET @err = 1;
-            IF @continueOnError = 0 
-            BEGIN
-				IF @debug=1
-					RAISERROR('Erreur lors de la tentative de tuer les sessions', 16, 1) WITH NOWAIT;
-                RETURN;
-            END
-			ELSE 
-			BEGIN
-				IF @debug = 1
-					 RAISERROR('Erreur lors de la tentative de suppression des sessions', 1, 1) WITH NOWAIT;
-			END
-        END CATCH*/
+        END
+        CLOSE session_cursor;
+        DEALLOCATE session_cursor;
     END
 
     -----------------------------------------------------------------------
-    -- 3. Exécuter la commande de suppression si @execute = 1
+    -- 3. Drop the snapshot database
     -----------------------------------------------------------------------
-    IF @err = 0 and @execute = 1
-    BEGIN
-        BEGIN TRY
-			IF @debug = 1
-				RAISERROR('Suppression du database snapshot %s', 1, 1, @dbsnapshotname) WITH NOWAIT;
-			SET @sql = 'DROP DATABASE '+QUOTENAME(@dbsnapshotname)+'';
-			PRINT @sql
-            EXEC sp_executesql @sql;
+    
+        BEGIN TRY			
+			SET @sql = 'DROP DATABASE '+ QUOTENAME(@dbsnapshotname) +';';
+			IF @debug=1
+				PRINT @sql;
+            IF @execute=1
+			BEGIN
+				EXEC sp_executesql @sql;
+				RAISERROR('Drop snapshot %s completed', 10, 1,@dbsnapshotname);
+			END
+			RETURN 0;
         END TRY
         BEGIN CATCH
-            SET @err = 1;
+			SET @ErrMsg = ERROR_MESSAGE();            
             IF @continueOnError = 0 
             BEGIN
-                RAISERROR('Erreur lors de la suppression du snapshot', 16, 1);
-                RETURN;
+                RAISERROR('Error when dropping the snapshot %s : %s', 10, 1,@dbsnapshotname,@ErrMsg);
+                RETURN 0;
             END
 			ELSE 
 			BEGIN
-				IF @debug=1
-					RAISERROR('Erreur lors de la suppression du database snapshot %s', 1, 1, @dbsnapshotname) WITH NOWAIT;	
+				RAISERROR('Error when dropping the snapshot  %s : %s', 16, 1, @dbsnapshotname,@ErrMsg) WITH NOWAIT;
+				RETURN 1;
 			END
 		END CATCH
-    END
-	
 END
